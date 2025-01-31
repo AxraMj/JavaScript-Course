7e8c4fb424101e15a7969c537307991b53863673
AxraMj <offxaxra@gmail.com>
2025-01-31 15:20:41 +0530

    01 commit

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -2,3 +2,10 @@ let js='Awsome';
 if (js=='Awsome') alert("Have Fun!!")
 
 console.log(20+10-10)
+
+// in js we use camelCase for variables
+let firstName ="John"
+let myVariableName = "Hello";
+let totalScore = 100;
+let userAge = 25;
+console.log(firstName)
