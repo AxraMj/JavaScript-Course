1efbd80bc4ae7f9744578ba43b6afa3f78c581e8
AxraMj <offxaxra@gmail.com>
2025-01-31 15:57:37 +0530

    01 commit

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -10,16 +10,33 @@
 // let userAge = 25;
 // console.log(firstName)
 
+let javaScriptIsFun=true
+let a= 12
+a="hello" //dynamic type
+let b= "John"
+let year; //undefined empty variable
+year=1991
+console.log(javaScriptIsFun)
+console.log(typeof javaScriptIsFun)
+console.log(typeof a)
+console.log(typeof b)
+console.log(year)
+console.log(typeof year)
+console.log(typeof null)
+//dynamic typing
+javaScriptIsFun = "YES!"
+console.log(javaScriptIsFun)
 
+// Assignment 1
 // Values and Variables
 // 1. Declare variables called 'country', 'continent' and 'population' and 
 // assign their values according to your own country (population in millions)
 // 2. Log their values to the console
 
-let country = "India"
-let continent ="Asia"
-let population = "142.86 Cr"
+// let country = "India"
+// let continent ="Asia"
+// let population = "142.86 Cr"
 
-console.log("Country: " +country)
-console.log("Continent: " +continent)
-console.log("Population: " +population)
+// console.log("Country: " +country)
+// console.log("Continent: " +continent)
+// console.log("Population: " +population)
