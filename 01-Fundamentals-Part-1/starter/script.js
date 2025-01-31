38648a87c36c2477f3c0edbf802832173c0991d1
AxraMj <offxaxra@gmail.com>
2025-01-31 15:28:25 +0530

    01 commit

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,11 +1,25 @@
-let js='Awsome';
-if (js=='Awsome') alert("Have Fun!!")
+// let js='Awsome';
+// if (js=='Awsome') alert("Have Fun!!")
 
-console.log(20+10-10)
+// console.log(20+10-10)
 
-// in js we use camelCase for variables
-let firstName ="John"
-let myVariableName = "Hello";
-let totalScore = 100;
-let userAge = 25;
-console.log(firstName)
+// // in js we use camelCase for variables
+// let firstName ="John"
+// let myVariableName = "Hello";
+// let totalScore = 100;
+// let userAge = 25;
+// console.log(firstName)
+
+
+// Values and Variables
+// 1. Declare variables called 'country', 'continent' and 'population' and 
+// assign their values according to your own country (population in millions)
+// 2. Log their values to the console
+
+let country = "India"
+let continent ="Asia"
+let population = "142.86 Cr"
+
+console.log("Country: " +country)
+console.log("Continent: " +continent)
+console.log("Population: " +population)
