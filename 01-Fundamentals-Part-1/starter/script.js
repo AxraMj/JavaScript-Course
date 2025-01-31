6f34fca1cf4d18975665e59ba5d0d7ffd8e7a4ec
AxraMj <offxaxra@gmail.com>
2025-01-31 16:08:59 +0530

    01 commit

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -10,6 +10,7 @@
 // let userAge = 25;
 // console.log(firstName)
 
+/*
 let javaScriptIsFun=true
 let a= 12
 a="hello" //dynamic type
@@ -22,21 +23,44 @@ console.log(typeof a)
 console.log(typeof b)
 console.log(year)
 console.log(typeof year)
-console.log(typeof null)
+console.log(typeof null) //object, actually its an error 
 //dynamic typing
 javaScriptIsFun = "YES!"
-console.log(javaScriptIsFun)
+console.log(javaScriptIsFun) 
+*/
 
 // Assignment 1
-// Values and Variables
-// 1. Declare variables called 'country', 'continent' and 'population' and 
-// assign their values according to your own country (population in millions)
-// 2. Log their values to the console
+/*
+Values and Variables
+1. Declare variables called 'country', 'continent' and 'population' and 
+assign their values according to your own country (population in millions)
+2. Log their values to the console
 
-// let country = "India"
-// let continent ="Asia"
-// let population = "142.86 Cr"
+let country = "India"
+let continent ="Asia"
+let population = "142.86 Cr"
 
-// console.log("Country: " +country)
-// console.log("Continent: " +continent)
-// console.log("Population: " +population)
+console.log("Country: " +country)
+console.log("Continent: " +continent)
+console.log("Population: " +population)
+*/
+
+/*
+LECTURE: Data Types
+1. Declare a variable called 'isIsland' and set its value according to your 
+country. The variable should hold a Boolean value. Also declare a variable
+'language', but don't assign it any value yet
+2. Log the types of 'isIsland', 'population', 'country' and 'language'
+to the console
+*/
+
+let country = "India"
+let continent ="Asia"
+let population = 142.86
+let isIsland = false
+let language;
+
+console.log(typeof isIsland)
+console.log(typeof continent)
+console.log(typeof population)
+console.log(typeof language)
