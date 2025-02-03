f14027a4c7dd37d64b7eeae84a891940e7659fb9
AxraMj <offxaxra@gmail.com>
2025-02-03 22:33:30 +0530

    02 commit

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -151,3 +151,28 @@ neighbours[neighbours.indexOf('France')]='Republic of France'
 console.log(neighbours)
 
 */
+/*
+LECTURE: Introduction to Objects
+1. Create an object called 'myCountry' for a country of your choice, containing 
+properties 'country', 'capital', 'language', 'population' and 
+'neighbours' (an array like we used in previous assignments)
+*/
+
+const myCountry={
+    country:'india',
+    captital:'New Delhi',
+    language:'Hindi',
+    population:3,
+    neighbours:["France", "Germany", "Italy"]
+}
+// LECTURE: Dot vs. Bracket Notation
+// 1. Using the object from the previous assignment, log a string like this to the 
+// console: 'Finland has 6 million finnish-speaking people, 3 neighbouring countries 
+// and a capital called Helsinki.'
+// 2. Increase the country's population by two million using dot notation, and then 
+// decrease it by two million using brackets notation
+
+console.log(`${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.captital}.`)
+
+console.log(myCountry.population+=2)
+console.log(myCountry['population']-=2)
