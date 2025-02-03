ade526ab6b15f7ee8d6811090e7682f34ba7073a
AxraMj <offxaxra@gmail.com>
2025-02-03 23:01:46 +0530

    02 commit

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -156,7 +156,7 @@ LECTURE: Introduction to Objects
 1. Create an object called 'myCountry' for a country of your choice, containing 
 properties 'country', 'capital', 'language', 'population' and 
 'neighbours' (an array like we used in previous assignments)
-*/
+
 
 const myCountry={
     country:'india',
@@ -165,6 +165,7 @@ const myCountry={
     population:3,
     neighbours:["France", "Germany", "Italy"]
 }
+
 // LECTURE: Dot vs. Bracket Notation
 // 1. Using the object from the previous assignment, log a string like this to the 
 // console: 'Finland has 6 million finnish-speaking people, 3 neighbouring countries 
@@ -176,3 +177,33 @@ console.log(`${myCountry.country} has ${myCountry.population} million ${myCountr
 
 console.log(myCountry.population+=2)
 console.log(myCountry['population']-=2)
+
+*/
+/*
+LECTURE: Object Methods
+1. Add a method called 'describe' to the 'myCountry' object. This method 
+will log a string to the console, similar to the string logged in the previous 
+assignment, but this time using the 'this' keyword.
+2. Call the 'describe' method
+3. Add a method called 'checkIsland' to the 'myCountry' object. This 
+method will set a new property on the object, called 'isIsland'. 
+'isIsland' will be true if there are no neighbouring countries, and false if 
+there are. Use the ternary operator to set the property
+*/
+const myCountry={
+    country:'india',
+    captital:'New Delhi',
+    language:'Hindi',
+    population:3,
+    neighbours:["France", "Germany", "Italy"],
+    isIsland: false,
+    describe: function (){
+        return `${this.country} has ${this.population} million ${this.language}-speaking people, ${this.neighbours.length} neighbouring countries and a capital called ${this.captital}.`
+    },
+    checkIsland:function (){
+        this.isIsland = this.neighbours.length === 0 ? true : false;
+    }
+}
+console.log(myCountry.describe()); // Logs the description of the country
+myCountry.checkIsland(); // Updates the 'isIsland' property based on neighbours
+console.log(myCountry.isIsland); // Logs whether the country is an island or not
