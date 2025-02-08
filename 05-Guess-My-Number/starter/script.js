e4b67e74dc5ed3e8b549789a67353b38f89986af
AxraMj <offxaxra@gmail.com>
2025-02-08 12:50:44 +0530

    Guess-my-number habdling click event

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,8 +1,19 @@
 'use strict';
-
+/*
 console.log(document.querySelector('.message').textContent);
 //selecting and manipulating elements
 document.querySelector('.message').textContent = 'Correct Guess..!';
 document.querySelector('.number').textContent = 9;
 document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 20;
+*/
+//handling click event
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    console.log('Not guesses');
+  }
+});
