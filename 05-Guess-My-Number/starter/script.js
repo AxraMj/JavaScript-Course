973dc63c52e37f9cfdae19c03e546ea31f9b2306
AxraMj <offxaxra@gmail.com>
2025-02-08 12:30:51 +0530

    Guess-my-number selecting and manipulating elements

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,3 +1,8 @@
 'use strict';
 
 console.log(document.querySelector('.message').textContent);
+//selecting and manipulating elements
+document.querySelector('.message').textContent = 'Correct Guess..!';
+document.querySelector('.number').textContent = 9;
+document.querySelector('.score').textContent = 10;
+document.querySelector('.guess').value = 20;
