ffaada801ddc65d12c46f6d29644d642b1c6910f
AxraMj <offxaxra@gmail.com>
2025-02-08 13:40:24 +0530

    Guess-my-number implementing the game logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -6,7 +6,7 @@ document.querySelector('.message').textContent = 'Correct Guess..!';
 document.querySelector('.number').textContent = 9;
 document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 20;
-*/
+
 //handling click event
 
 document.querySelector('.check').addEventListener('click', function () {
@@ -15,5 +15,40 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guess) {
     console.log('Not guesses');
+  }
+});
+
+*/
+
+//implementing the game logic
+const secretNumber = Math.round(Math.random() * 20);
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'Not Guessed';
+  } else if (guess == secretNumber) {
+    document.querySelector('.message').textContent = 'Currect Guess!!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      19 > 0;
+      document.querySelector('.message').textContent = 'Too high!!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Fail!!';
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      19 > 0;
+      document.querySelector('.message').textContent = 'Too Low!!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Fail!!';
+    }
   }
 });
