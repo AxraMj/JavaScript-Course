2267ebcb9464fcf3786cd24a20a481a2aa3dc6e4
AxraMj <offxaxra@gmail.com>
2025-02-11 22:20:47 +0530

    4th commit

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -5,15 +5,18 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal'); // Store the NodeList correctly
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('button clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
 const closeModel = () => {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
 btnCloseModal.addEventListener('click', closeModel);
 overlay.addEventListener('click', closeModel);
