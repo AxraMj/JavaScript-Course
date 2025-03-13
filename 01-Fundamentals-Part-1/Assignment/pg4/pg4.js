af6640f566738a5eb7b622bbf71dbdbb13f4b66a
AxraMj <offxaxra@gmail.com>
2025-03-13 12:08:26 +0530

    Assignment pg5

--- 01-Fundamentals-Part-1/Assignment/pg4/pg4.js
+++ 01-Fundamentals-Part-1/Assignment/pg4/pg4.js
@@ -16,22 +16,30 @@ let population = 1_400_000_000; // India's actual population in numbers
 const isIsland = false; // India is not an island
 let language = "Hindi";
 
+// 2. Split the population in half
 let half_Population = population / 2;
-console.log(`one half will contain ${half_Population} Population`);
+console.log(`One half will contain ${half_Population} people`);
+
+// 3. Increase the population by 1
 population += 1;
-console.log(population);
+console.log(`Updated population: ${population}`);
 
+// 4. Compare with Finland's population (6 million)
 if (population > 6_000_000) {
-  console.log(`${country} has Highest Population than Finland`);
+  console.log(`${country} has a higher population than Finland.`);
 } else {
-  console.log(`${country} has Less Population than Finland`);
+  console.log(`${country} has a lower population than Finland.`);
 }
 
-if (half_Population > 33_000_000) {
-  console.log(`${country} has Highest Population than Avrg`);
+// 5. Compare with the average country population (33 million)
+if (population > 33_000_000) {
+  console.log(`${country} has a higher population than the average country.`);
 } else {
-  console.log(`${country} has Less Population than Avrg`);
+  console.log(`${country} has a lower population than the average country.`);
 }
 
-let description = `${country} is in ${continent}, and its ${half_Population} million people speak ${language}`;
+// 6. Create the description string
+let description = `${country} is in ${continent}, and its ${
+  population / 1_000_000
+} million people speak ${language}.`;
 console.log(description);
