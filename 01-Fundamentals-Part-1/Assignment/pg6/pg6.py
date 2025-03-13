ae41e6283e969a7e4205bea00805b4055bfa7fd3
AxraMj <offxaxra@gmail.com>
2025-03-13 12:23:03 +0530

    Assignment pg6

---
+++ 01-Fundamentals-Part-1/Assignment/pg6/pg6.py
@@ -0,0 +1,10 @@
+# Predict the result of these 5 operations without executing them: 
+# '9' - '5'; 
+# '19' - '13' + '17'; 
+# '19' - '13' + 17; 
+# '123' < 57; 
+# 5 + 6 + '4' + 9 - 4 - 2; 
+# 2. Execute the operations to check if you were right
+
+print(int('9')-int('5'))  #output 4
+print(int('19') - int('13') + int('17'))
