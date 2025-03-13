1b9e41b7aa9352eb3a9dfa51de8c95491246583f
AxraMj <offxaxra@gmail.com>
2025-03-13 11:15:47 +0530

    Assignment pg2

---
+++ 01-Fundamentals-Part-1/Assignment/pg2/pg2.js
@@ -0,0 +1,18 @@
+// 1. Declare a variable called 'isIsland' and set its value according to your
+// country. The variable should hold a Boolean value. Also declare a variable
+// 'language', but don't assign it any value yet
+// 2. Log the types of 'isIsland', 'population', 'country' and 'language'
+// to the console
+
+// 1. Declare variables
+let country = "India";
+let continent = "Asia";
+let population = 1_400_000_000; // India's actual population in numbers
+let isIsland = false; // India is not an island
+let language; // Not assigned yet
+
+// 2. Log the types of each variable
+console.log(typeof isIsland); // boolean
+console.log(typeof population); // number
+console.log(typeof country); // string
+console.log(typeof language); // undefined
---
+++ 01-Fundamentals-Part-1/Assignment/pg2/pg2.py
@@ -0,0 +1,17 @@
+# 1. Declare a variable called 'isIsland' and set its value according to your 
+# country. The variable should hold a Boolean value. Also declare a variable 
+# 'language', but don't assign it any value yet 
+# 2. Log the types of 'isIsland', 'population', 'country' and 'language' 
+# to the console
+
+country='india'
+continent='Asia'
+population=1_400_000_000; 
+isIsland =False
+language=None
+
+print(type(country))
+print(type(continent))
+print(type(population))
+print(type(isIsland ))
+print(type(language))
