2d5e21d77182424cfbfb015ea98a53bdefda86fc
AxraMj <offxaxra@gmail.com>
2025-03-13 11:02:09 +0530

    Assignment pg1

---
+++ 01-Fundamentals-Part-1/Assignment/pg1/pg1.js
@@ -0,0 +1,12 @@
+// LECTURE: Values and Variables
+// 1. Declare variables called 'country', 'continent' and 'population'
+// and assign their values according to your own country (population in millions)
+// 2. Log their values to the console
+
+let country = "India";
+let continent = "Asia";
+let population = 1_400_000_000; // India's actual population in numbers
+
+console.log(
+  `My country is ${country}, it is in ${continent}, and its population is ${population}.`
+);
