b2d01c043b6d5b0cafd2bfa59cf53b309e2d1038
AxraMj <offxaxra@gmail.com>
2025-03-13 11:05:34 +0530

    Assignment pg1

---
+++ 01-Fundamentals-Part-1/Assignment/pg1/pg1.py
@@ -0,0 +1,10 @@
+# // LECTURE: Values and Variables
+# // 1. Declare variables called 'country', 'continent' and 'population'
+# // and assign their values according to your own country (population in millions)
+# // 2. Log their values to the console
+
+country='india'
+continent='Asia'
+population=1_400_000_000; 
+
+print(f'My country is {country}, it is in {continent}, and its population is {population}.')
